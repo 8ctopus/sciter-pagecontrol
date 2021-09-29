5dbaf796c88095b4e0865b3518c9de6bfae79788
8ctopus <hello@octopuslabs.io>
2021-09-29 11:41:53 +0500

    updated: spm to 0.1.6

--- install.bat
+++ install.bat
@@ -3,7 +3,7 @@ curl -LO https://github.com/c-smile/sciter-js-sdk/raw/44c64a795c60147fb832cceda2
 curl -LO https://github.com/c-smile/sciter-js-sdk/raw/44c64a795c60147fb832cceda2a939ac8c0d7e24/bin/windows/x32/inspector.exe
 curl -LO https://github.com/c-smile/sciter-js-sdk/raw/44c64a795c60147fb832cceda2a939ac8c0d7e24/bin/windows/x32/sciter.dll
 
-REM sciter package manager 0.1.5
-curl -LO https://github.com/8ctopus/sciter-package-manager/releases/download/0.1.5/spm.phar
+REM sciter package manager 0.1.6
+curl -LO https://github.com/8ctopus/sciter-package-manager/releases/download/0.1.6/spm.phar
 
 pause
